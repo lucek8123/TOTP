43a143ec7509b8c0f26718578cc278f69346f4ee
Lucjan Krzywdziński <lucjan.krzywdzinski@icloud.com>
2023-08-29 21:29:30 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,9 @@
+#  TOTP App
+
+It is simple project and now it doesn't have file protection, and please **dont** use it for app for generating one time passwords for your accounts. I dont take responsibility for any possible data leaks.
+
+To do:
+ - [x] Icons
+ - [ ] Use MVVM
+ - [ ] Password protection 
+ - [ ] iCloud Sync (maybe in future)
--- TOTP/Views/AcknowledgementsView.swift
+++ TOTP/Views/AcknowledgementsView.swift
@@ -18,9 +18,6 @@ struct AcknowledgementsView: View {
                     Text("[FaviconFinder](https://github.com/will-lumley/FaviconFinder) by [Will Lumley](https://github.com/will-lumley)")
                     Text("[CodeScanner](https://github.com/twostraws/CodeScanner) by [Paul Hudson](https://github.com/twostraws)")
                 }
-                Section("Sepecial Thanks") {
-                    Text("Special thanks for both of my parents because they supported me through my programming journey. For my dad because he inspired me to programming and with him I started learning swift. \n\nFor Paul Hudson (@twostraws) because I learned SwiftUI with his free 100 Days with SwiftUI course.")
-                }
             }
             .navigationTitle("Acknowledgements")
         }
